fb7154b881e1f7a04e15e87bbeab7e278c27a703
mandonglawrence <mandonglawrence@gmail.com>
2021-09-06 14:52:57 +0100

    quick fix

--- src/components/ProgressBar-H.js
+++ src/components/ProgressBar-H.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styledComponents from '../styledComponents';
+import styledComponents from './styledComponents';
 
 export default function ProgressBar({ value, max, progressColor,progressBackgroundColor,progressValueColor,progressValueSize, width,height}) {
     return (
