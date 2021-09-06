4d77bbc9b78409bb95a720c9b621d47dee7de560
ifedayo <ifedayoagboola19@gmail.com>
2021-09-06 01:00:53 +0100

    build: input components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import InputPhone from "./components/forms/InputPhone";
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Sabi-Agent Web</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<h1>Sabi-Agent Web</h1>
+			<div>
+				<InputPhone />
+			</div>
+		</div>
+	);
 }
 
 export default App;
